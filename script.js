4c2b69c24a4ceafa2db3035385806d5822bc46fa
Anthony Luong <anthony@Anthonys-MacBook-Pro.local>
2023-01-09 13:34:35 -0500

    Initial Push

--- script.js
+++ script.js
@@ -11,3 +11,27 @@
 </ul>
 <img src="${}">
 */
+
+
+
+// TODO 1 : set up a window load handler
+window.addEventListener("load", function(){
+
+})
+   // TODO 2: set upa submit handler for the form
+
+      // TODO 3: cancel submission using event.preventDefault()
+
+      // TODO 4: Validate that all inputs have data
+
+      // TODO 5: Check fuel level and cargo mass, and report launch status
+
+      // TODO 6: make the list visible
+
+
+
+   // TODO 7: Fetch planet data
+
+      // TODO 8: randomly choose one of the planets
+
+      // TODO 9: display info about the chosen planets
